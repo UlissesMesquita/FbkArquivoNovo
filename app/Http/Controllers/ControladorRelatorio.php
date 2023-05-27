64d519eb188b48a80d259cfe0d5a8bd74eadd34d
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2023-05-27 11:34:08 -0300

    trycath export pdf

--- app/Http/Controllers/ControladorRelatorio.php
+++ app/Http/Controllers/ControladorRelatorio.php
@@ -443,24 +443,21 @@ class ControladorRelatorio extends Controller
     }
 
     public function exportPdf(Request $request){
+        
+
         $dashIds = $request->dash_id;
-        
-        if (isNull($dashIds) || $dashIds = '') {
-            return "Sem dados na requição";
+    
+        try{
+            $cadastros = Cadastro_Documentos::wherein('id_codigo', $dashIds)->get();
+
+            return $pdf = Pdf::set_option('isHtml5ParserEnabled', true)
+                            ->setPaper('a4', 'landscape')
+                            ->loadview('pdfs.pdf', ['cadastros' => $cadastros])
+                            ->download('Relatorio_Geral_'.date("d-m-Y__H-i").'.pdf');
+
         }
-        else {
-        
-        $cadastros = Cadastro_Documentos::wherein('id_codigo', $dashIds)->get();
-
-        //$pdf = PDF::loadView('pdfs.pdf', compact('cadastros'));
-        $pdf = Pdf::loadView('pdfs.pdf', ['cadastros' => $cadastros]);
-
-        $pdf->set_option(
-            'isHtml5ParserEnabled', true)->
-            setPaper('a4', 'landscape');
-
-        //return $pdf->stream('Relatorio_Geral_'.date("d-m-Y__H-i").'.pdf');
-        return $pdf->download('Relatorio_Geral_'.date("d-m-Y__H-i").'.pdf');
+        catch (\Exception $e){
+            echo "Error: ". $e;
         }
     }
 
