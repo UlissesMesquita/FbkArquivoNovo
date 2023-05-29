7f9215bcc1fb96fce409308e1be452706da4e283
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2023-05-29 19:05:11 -0300

    Dnginx

--- app/Http/Controllers/ControladorRelatorio.php
+++ app/Http/Controllers/ControladorRelatorio.php
@@ -457,6 +457,7 @@ class ControladorRelatorio extends Controller
             $css .= "";
 
             $ValorTotal = number_format($ValorTotal, 2, ',',".");
+            define("DOMPDF_ENABLE_REMOTE", true);
             return $pdf = Pdf::set_option('isHtml5ParserEnabled', true)
                             ->setPaper('a4', 'landscape')
                             ->loadview('pdfs.pdf', 
--- resources/views/pdfs/pdf.blade.php
+++ resources/views/pdfs/pdf.blade.php
@@ -13,7 +13,8 @@
     <meta charset="utf-8">
     <title>  RELATÓRIO DE CONFERÊNCIA DE ENTRADA DE LOTE </h2></title>
 
-    <img src="{{Storage::path('logos/cientik.jpg')}}">
+    <img src="logos/cientik.jpg">
+    {{-- {{dd(Storage::path('logos/cientik.jpg'))}} --}}
     <img src="{{asset('logos/conecta.jpg')}}">
     <img src="{{asset('logos/fabrika.jpg')}}">
     <img src="{{asset('logos/loccamera.jpg')}}">
