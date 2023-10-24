5024e4aa6342c0e0a9f80c4a9a817e8ff25b5070
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2023-10-24 12:02:26 -0300

    Teste

--- app/Http/Controllers/ControladorRelatorio.php
+++ app/Http/Controllers/ControladorRelatorio.php
@@ -33,7 +33,7 @@ class ControladorRelatorio extends Controller
                 //Query para utilização sem paginação
                     //$dash = Cadastro_Documentos::all()->sortByDesc('id_codigo');
                 //Query para apaginação
-                $dash = Cadastro_Documentos::orderBy('id_codigo', 'DESC')->limite(200)->Paginate(200);
+                $dash = Cadastro_Documentos::orderBy('id_codigo', 'DESC')->limit(200)->Paginate(200);
                 //dd($dash);
                 //$dash = Cadastro_Documentos::paginate();
                 
