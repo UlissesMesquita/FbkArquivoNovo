e3a373eb241e4825f794d5010576cbe381f87c8e
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2023-10-28 20:18:36 -0300

    Fix upload icon

--- app/Http/Controllers/ControladorRelatorio.php
+++ app/Http/Controllers/ControladorRelatorio.php
@@ -158,7 +158,7 @@ class ControladorRelatorio extends Controller
             ->where('cad_departamento', '=', 'OUTROS')
             ->get();    
 
-            $anexos = Upload::select('id_upload_codigo')->groupBy('id_upload_codigo')->get()->paginate(100);
+            $anexos = Upload::select('id_upload_codigo')->groupBy('id_upload_codigo')->paginate(100)->get();
             dd($anexos);
             return view('forms_reports/documentos_search_reports', compact(
             'contador',
