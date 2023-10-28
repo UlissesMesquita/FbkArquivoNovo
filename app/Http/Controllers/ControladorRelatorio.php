bf03702ddbecfaa5e3b5023af2790eea137b6a1e
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2023-10-28 19:35:32 -0300

    paginate - upload

--- app/Http/Controllers/ControladorRelatorio.php
+++ app/Http/Controllers/ControladorRelatorio.php
@@ -158,7 +158,7 @@ class ControladorRelatorio extends Controller
             ->where('cad_departamento', '=', 'OUTROS')
             ->get();    
 
-            $anexos = Upload::select('id_upload_codigo')->distinct()->get();
+            $anexos = Upload::select('id_upload_codigo')->distinct()->get()->Paginate(100);
 
             
             return view('forms_reports/documentos_search_reports', compact(
